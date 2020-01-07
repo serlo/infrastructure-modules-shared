c331726b68a536449f88960458c6cb4297d6be46
Jonas Keinholz <git@inyono.dev>
2020-01-07 14:12:20 +0100

    add redis

---
+++ redis/main.tf
@@ -0,0 +1,35 @@
+resource "helm_release" "redis" {
+  name       = "redis"
+  chart      = "stable/redis"
+  repository = data.helm_repository.stable.metadata[0].name
+  namespace  = var.namespace
+
+  set {
+    name  = "image.tag"
+    value = var.image_tag
+  }
+
+  set {
+    name = "cluster.enabled"
+    value = true
+  }
+
+  set {
+    name = "cluster.slaveCount"
+    value = 0
+  }
+
+  set {
+    name = "usePassword"
+    value = false
+  }
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com/"
+}
+
+provider "helm" {
+  version = "~> 0.10"
+}
---
+++ redis/variables.tf
@@ -0,0 +1,9 @@
+variable "namespace" {
+  description = "Kubernetes namespace to use"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Rocket Chat image tag to use"
+  type        = string
+}
