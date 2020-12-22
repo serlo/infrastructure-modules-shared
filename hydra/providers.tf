19929bd5eb46b09dbacb69df9d228d4f39008317
Jonas Keinholz <git@inyono.dev>
2020-12-22 13:57:44 +0100

    update helm provider

--- hydra/providers.tf
+++ hydra/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 1.2"
+      version = "~> 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
