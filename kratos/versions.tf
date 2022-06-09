79d746f19db90d06a5c99115a8676a74fb7658e3
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-09 14:14:52 +0200

    feat: reput kratos module
    See 29ca8c3a
    
    Co-authored-by: inyono <git@inyono.dev>

---
+++ kratos/versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0"
+    }
+  }
+}
