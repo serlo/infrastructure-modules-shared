87f5935e629d0c8b3fa5f87edf2e2aab3662cdb2
Jonas Keinholz <git@inyono.dev>
2022-06-10 02:21:19 +0200

    ingress-nginx: fix versions.tf

--- ingress-nginx/versions.tf
+++ ingress-nginx/versions.tf
@@ -2,12 +2,8 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     kubernetes = {
-      source  = "hashicorp/kubernetes"
+      source  = "hashicorp/helm"
       version = ">= 2.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.0"
     }
   }
 }
