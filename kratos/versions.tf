5a35272ea6f11d4255be7a6a80db9e9d8aefe523
Jonas Keinholz <git@inyono.dev>
2022-06-13 15:15:29 +0200

    kratos: migrate to updated Ingress API

--- kratos/versions.tf
+++ kratos/versions.tf
@@ -13,10 +13,6 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 2.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 3.0"
