26b0ea63a38582dfd0a79aca3c84a7ddb4755f59
Andreas Huber <andy.huber@tum.de>
2023-04-17 08:26:43 +0200

    feat(registration): replace http call for body with base64 encoded function

--- kratos/main.tf
+++ kratos/main.tf
@@ -56,6 +56,7 @@ resource "helm_release" "kratos_deployment" {
         nbp_client_secret = var.nbp_client_secret != "" ? var.nbp_client_secret : "we have no secret"
         sso_enabled       = var.nbp_client_secret != "" ? true : false
         mapper            = base64encode(file("${path.module}/user_mapper.jsonnet"))
+        user_id           = base64encode("function (ctx) { userId: ctx.identity.id }")
       }
     )
   ]
